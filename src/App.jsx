61ab61a59755beac7c7f3388f5ed87c024d01df5
chandanr54 <67356198+chandanr54@users.noreply.github.com>
2026-04-16 19:49:06 +0530

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -8,7 +8,7 @@ function App() {
   const [count, setCount] = useState(0)
 
   return (
-    <>
+    <> chandan
       <section id="center">
         <div className="hero">
           <img src={heroImg} className="base" width="170" height="179" alt="" />
