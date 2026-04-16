745c6df0a63ff96208f6dfad20d29030344b619c
chandanr54 <67356198+chandanr54@users.noreply.github.com>
2026-04-16 20:12:44 +0530

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -4,7 +4,7 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: '/react_rect_CICD_github/',
+  base: '/react_CICD/',
   server: {
     port: 3001,
     open: true,
